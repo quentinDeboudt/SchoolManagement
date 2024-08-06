e6a3c5910fd4c293d9b426e4e5e8b4faf7f6f3b5
quentinDeboudt <Deboudtquentin@gmail.com>
2024-08-06 17:01:37 +0200

    (feature/#1_add_new_BDD) config

--- SchoolManagement_back/obj/Debug/net8.0/school_calendar_back.AssemblyInfo.cs
+++ SchoolManagement_back/obj/Debug/net8.0/school_calendar_back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("school_calendar_back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+145311dc08f60264f67e87118813a52c5d6a0649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8877b640fc736a9b18400ca507846804352c7bd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("school_calendar_back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("school_calendar_back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
