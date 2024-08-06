4b2cdfeccdfb9e030bfd7b0a195c9e1d3860115f
quentinDeboudt <Deboudtquentin@gmail.com>
2024-08-06 13:07:01 +0200

    (feature/#1_create_entities) Add Role

--- SchoolManagement_back/SchoolManagement.Domain/Entities/Role.cs
+++ SchoolManagement_back/SchoolManagement.Domain/Entities/Role.cs
@@ -1,7 +1,8 @@
-namespace Domain.Entities;
+namespace SchoolManagement.Domain.Entities;
 
 public class Role
 {
-    public Guid Id {get; set; }
-    public required string Name {get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<PersonRole> PersonRoles { get; set; }
 }
