54251286b6a2a91e3d874660fd69c11a77e957e7
quentinDeboudt <Deboudtquentin@gmail.com>
2024-08-07 12:26:03 +0200

    Retirer les fichiers générés du contrôle de version

--- SchoolManagement_back/obj/Debug/net8.0/school_calendar_back.AssemblyInfo.cs
+++ SchoolManagement_back/obj/Debug/net8.0/school_calendar_back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("school_calendar_back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b847e9098f363a819fee2eb8ed1f3accecaf2a5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f4c3ee0fad12850083849627f78143999090f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("school_calendar_back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("school_calendar_back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
