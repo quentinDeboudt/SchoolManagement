41236ce247a0ffa8b2c1d13dc9cb8dfa2b788f95
quentinDeboudt <Deboudtquentin@gmail.com>
2024-08-07 18:47:46 +0200

    (feature/#4_Import_Angular_Materials) Add component dashboard

--- SchoolManagement_front/src/app/dashboard/dashboard.component.ts
+++ SchoolManagement_front/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatIcon
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
