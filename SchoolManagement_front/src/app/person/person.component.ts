6865c768968f4541c90d7522caf4ca411ab61eb3
quentinDeboudt <Deboudtquentin@gmail.com>
2024-08-08 13:12:01 +0200

    (feature/#5_designe_and_other_page) get person list with DB

--- SchoolManagement_front/src/app/person/person.component.ts
+++ SchoolManagement_front/src/app/person/person.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { GenericTableComponent } from '../generic/generic-table/generic-table.component';
+import { Person } from '../../models/person.model';
+import { PersonService } from '../../service/person.service';
 
 @Component({
   selector: 'app-person',
@@ -18,6 +20,19 @@ import { GenericTableComponent } from '../generic/generic-table/generic-table.co
   templateUrl: './person.component.html',
   styleUrl: './person.component.scss'
 })
-export class PersonComponent {
+export class PersonComponent implements OnInit{
 
+  persons: Person[] = [];
+
+  constructor(private personService: PersonService) {
+    console.log(this.persons);
+  }
+
+  ngOnInit(): void {
+    this.getPersons();
+  }
+
+  getPersons(): void {
+    this.personService.getPersons().subscribe(persons => this.persons = persons);
+  }
 }
