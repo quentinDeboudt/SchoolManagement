b45c8a636971598725fde949ec7779786fb162bb
quentinDeboudt <Deboudtquentin@gmail.com>
2024-08-09 09:49:46 +0200

    (feature/#5_designe_and_other_page) modify dashboard

--- SchoolManagement_front/src/app/dashboard/dashboard.component.ts
+++ SchoolManagement_front/src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,18 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
     MatGridListModule,
     MatCardModule,
-    MatIcon
+    MatIcon,
+    MatProgressSpinnerModule,
+    MatProgressBarModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
